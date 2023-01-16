e08238de77c8f4aef0c7eaa451cd134383d88666
Kajsia Anderson <kajsia.anderson@gmail.com>
2023-01-16 14:49:15 -0700

    added user name in profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import styles from './Profile.module.css'
+// import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import AuthContext from '../../store/auth-context'
+import User from './User'
 
 const Profile = () => {
+  const {userId} = useContext(AuthContext)
+  const [user, setUser] = useState([])
+  // const { id } = useParams()
+
+
+  useEffect(() => {
+      axios.get(`/user/${userId}`)
+      .then((res) => {
+          setUser(res.data)
+      })
+      .catch(err => {
+          console.log('error getting user', err)
+      })
+}, [userId])
+
+  useEffect(() => {
+      console.log('user', user);
+  }, [user])
+
+  const mappedUser = user.map(user => {
+    return <User
+      id={user?.id}
+      key={user?.id}
+      firstname={user?.firstname}
+    />
+})
+
   return (
-    <div className={styles.profile}>Profile</div>
+    <div className={styles.profile}>
+      <div className={styles.kookie}>{userId}</div>
+      <div className={styles.kookie}>Welcome {user.length !== 0 && user[0].firstname}</div>
+      <div className={styles.kookie}>{mappedUser}</div>
+    </div>
   )
 }
 
